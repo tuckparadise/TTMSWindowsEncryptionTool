d5505fe3ed1432f8513faa7989e5785ace8c4915
JasonAng-cloud <70874034+JasonAng-cloud@users.noreply.github.com>
2022-02-15 01:51:29 +0800

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TTMSWindowsEncryptionTool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEncryptedText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plain Text:";
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Location = new System.Drawing.Point(112, 10);
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(199, 20);
+            this.txtPlainText.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Encrypted Text:";
+            // 
+            // txtEncryptedText
+            // 
+            this.txtEncryptedText.Location = new System.Drawing.Point(112, 45);
+            this.txtEncryptedText.Multiline = true;
+            this.txtEncryptedText.Name = "txtEncryptedText";
+            this.txtEncryptedText.ReadOnly = true;
+            this.txtEncryptedText.Size = new System.Drawing.Size(654, 65);
+            this.txtEncryptedText.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtEncryptedText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TTMS Windows Encryption Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEncryptedText;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TTMSWindowsEncryptionTool
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string error = "";
+            if (txtPlainText.Text.Trim() =="")
+            {
+                error = "Plain Text Field cannot be blank";                
+            }
+
+            if (error != "")
+            {
+                MessageBox.Show(error);                
+            }
+            else
+            {
+                Process compiler = new Process();
+                compiler.StartInfo.FileName = "TTMSEncryptText.exe";
+                compiler.StartInfo.Arguments = "encrypt " + txtPlainText.Text.Trim();
+                compiler.StartInfo.UseShellExecute = false;
+                compiler.StartInfo.RedirectStandardError = true;
+                compiler.StartInfo.RedirectStandardOutput = true;
+                compiler.Start();
+
+                string result = compiler.StandardOutput.ReadToEnd();
+
+                compiler.WaitForExit();
+
+                if (compiler.ExitCode == 0)
+                {
+                    txtEncryptedText.Text = result.Trim();
+                }
+                else
+                {
+                    MessageBox.Show("Unable to get the result");
+                }
+
+                /*
+                Process p = Process.Start("TTMSEncryptText.exe " + txtPlainText.Text.Trim());
+                p.BeginOutputReadLine();
+                p.WaitForExit();
+                Console.WriteLine("myapp.exe returned {0}", p.ExitCode);
+                if (p.ExitCode == 0)
+                {
+
+                }
+                */
+            }
+        }
+    }
+}
